53f4df2c25c8238f23c8f519aff8f7cdd9aed1df
Pavel Durov <p3ld3v@pm.me>
2023-07-11 11:23:27 +0100

    Build script clone yk

--- .buildbot.sh
+++ .buildbot.sh
@@ -2,23 +2,23 @@
 
 set -eu
 
-# rm -fr ./build && mkdir ./build 
-# cd build
+rm -fr ./build && mkdir ./build 
+cd build
 
-# export CARGO_HOME="${PWD}/.cargo"
-# export RUSTUP_HOME="${PWD}/.rustup"
+export CARGO_HOME="${PWD}/.cargo"
+export RUSTUP_HOME="${PWD}/.rustup"
 
 # install rust
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
-# sh rustup.sh --default-host x86_64-unknown-linux-gnu --default-toolchain stable -y --no-modify-path
-# export PATH="${PWD}/.cargo/bin/:$PATH"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
+sh rustup.sh --default-host x86_64-unknown-linux-gnu --default-toolchain stable -y --no-modify-path
+export PATH="${PWD}/.cargo/bin/:$PATH"
 
 # yk bulid
-# git clone git@github.com:ykjit/yk.git 
-# git submodule update --init --recursive
-# cd yk
-# cargo build
-# cd ..
+git clone git@github.com:ykjit/yk.git 
+git submodule update --init --recursive
+cd yk
+cargo build
+cd ..
 
 # yklua bulid
 # git clone git@github.com:ykjit/yklua.git
